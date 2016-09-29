344c78e66e9aef16f4cbca115af726925e566d4b
GCorbel <guirec.corbel@gmail.com>
2016-09-29 08:32:43 -0400

    Autosync 2016-09-29T08-0400

--- autosync.sh
+++ autosync.sh
@@ -2,10 +2,10 @@
 
 # Add it in a cron
 # crontab -e
-# * * * * * dougui cd ~/.dotfiles && sh autosync.sh
+# * * * * * sh autosync.sh
 
 #Pull from remote repo
 git pull
 
 #Push changes
-git add -A && git commit -m "Sync $(date --iso-8601=hours)" && git push
+git add -A && git commit -m "Autosync $(date --iso-8601=hours)" && git push
